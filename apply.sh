23c391a58622d2dfcd52f10d8f8c11701071ad3a
MeexReay <meexreay@gmail.com>
2025-05-29 15:07:36 +0300

    waybar config gix

--- apply.sh
+++ apply.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
 cp aliases ~/.bash_aliases
-cp -a lightdm/. /etc/lightdm
+sudo cp -a lightdm/. /etc/lightdm
 cp -a kitty/. ~/.config/kitty
 cp -a sway/. ~/.config/sway
 cp -a waybar/. ~/.config/waybar
 cp -a helix/. ~/.config/helix
 cp -a dunst/. ~/.config/dunst
-chmod +x ~/.startup.sh
+chmod +x ~/.startup
 chmod +x ~/.bash_aliases
